9370822a89417f62f8fbab874a328531a4c36545
Nik-VYb <yyovvouk@gmail.com>
2025-10-24 11:38:51 +0100

    Add NotFound component for handling 404 errors

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export const runtime = 'edge';
+
+export default function NotFound() {
+  return (
+    <main className="prose mx-auto p-6">
+      <h1>Not found</h1>
+      <p>That page doesn’t exist.</p>
+    </main>
+  );
+}
